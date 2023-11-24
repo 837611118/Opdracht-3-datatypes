86ad41817352784cbb827da18e617ad9bc52fbd0
837611118 <144031540+837611118@users.noreply.github.com>
2023-11-24 09:29:59 +0100

    Add files via upload

---
+++ opdracht.php
@@ -0,0 +1,18 @@
+<?php
+
+$string[1] ="dat is een zin 1";
+
+$string [2] ="dat is een zin 2";
+
+$string[3] ="1";
+
+$string[4] ="2"; 
+  
+echo $string[1] ;
+echo "<br>";
+echo $string [2] ;
+echo "<br>";
+echo $string[3];
+echo "<br>";
+echo  $string[4] ;
+?>
